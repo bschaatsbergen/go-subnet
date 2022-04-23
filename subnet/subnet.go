baf9eee15da5a63612dfeb741d8cd87cd25e14ce
Bruno Schaatsbergen <git@bschaatsbergen.com>
2022-04-23 13:48:27 +0200

    Add `GetAvailableHostAddressesCount` func

--- subnet/subnet.go
+++ subnet/subnet.go
@@ -1,1 +1,17 @@
 package subnet
+
+import (
+	"math"
+	"net"
+)
+
+// GetAvailableHostAddresses returns the number of available host addresses in a subnet.
+func GetAvailableHostAddressesCount(base *net.IPNet) int {
+	mask := base.Mask
+
+	parentLen, addrLen := mask.Size()
+	hostLen := addrLen - parentLen
+
+	// The number of available host addresses is the number of host addresses minus the network address and broadcast address.
+	return int(math.Pow(2, float64(hostLen)) - 2)
+}
--- subnet/subnet_test.go
+++ subnet/subnet_test.go
@@ -1,1 +1,37 @@
 package subnet
+
+import (
+	"net"
+	"testing"
+)
+
+func TestGetAvailableHostAddressesCount(t *testing.T) {
+	type Case struct {
+		Base     string
+		Expected int
+	}
+
+	cases := []Case{
+		{
+			Base:     "10.0.0.0/16",
+			Expected: 65534,
+		},
+		{
+			Base:     "192.0.2.0/24",
+			Expected: 254,
+		},
+		{
+			Base:     "66.100.50.0/11",
+			Expected: 2097150,
+		},
+	}
+
+	for _, testCase := range cases {
+		_, base, _ := net.ParseCIDR(testCase.Base)
+		result := GetAvailableHostAddressesCount(base)
+
+		if result != testCase.Expected {
+			t.Errorf("Expected %d, got %d", testCase.Expected, result)
+		}
+	}
+}
